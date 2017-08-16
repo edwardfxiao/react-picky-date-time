5ec777b26ba147e5d5d7063a33ff4d4255855974
edward <edwardfhsiao@gmail.com>
2017-08-16 13:24:33 +0800

    no message

---
+++ src/css/variables/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /* ===== shadow ===== */
+  shadow1: '1px 1px 2px 0 rgba(0, 0, 0, 0.1)',
+  shadow2: '1px 1px 4px 0 rgba(0, 0, 0, 0.1)',
+  shadow3: '1px 1px 8px 0 rgba(0, 0, 0, 0.1)',
+  shadow4: '2px 2px 8px 0 rgba(0, 0, 0, 0.1)',
+  shadow5: '2px 2px 8px 0 rgba(0, 0, 0, 0.2)',
+  shadow6: '2px 2px 8px 0 rgba(0, 0, 0, 0.3)',
+  shadow7: '2px 2px 8px 0 rgba(0, 0, 0, 0.5)',
+
+  shadowCard: '0 8px 10px 0 rgba(0, 0, 0, 0.12), 0 0 10px 0 rgba(0, 0, 0, 0.08)',
+
+  shadowInput: '1px 1px 4px 0 rgba(0, 0, 0, 0.10), 0 0 4px 0 rgba(0, 0, 0, 0.08)',
+  shadowInputHover: '0px 0px 2px 0 rgba(0, 0, 0, 0.10), 0 0 2px 0 rgba(0, 0, 0, 0.08)',
+  shadowInputError: '0px 0px 2px 0 rgba(0, 0, 0, 0.10), 0 0 2px 0 rgba(240, 62, 62, 1)',
+
+  /* ===== color ===== */
+  bg: '#F7F8F9',
+  bgDark: '#252525',
+  bgDeep: '#eaeaea',
+  flatCard: '#F4F4F4',
+  flatCardBorder: '#D5D5D5',
+
+  /* ===== zIndex ===== */
+  zIndexHidden: '-9',
+  zIndex0: '-1',
+  zIndex1: '1',
+  zIndex9: '9',
+  zIndex99: '99',
+  zIndex999: '999',
+
+  // input
+  inputHeight: '30px'
+};
