90257a3f59068530fd40ca2f3e121c39dd22b8ac
Edward Xiao <edwardfhsiao@gmail.com>
2018-02-05 16:21:42 +0800

    no message

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-// var PickyDateTime = require('./lib/components/index.js').default;
-// require('./lib/react-picky-date-time.min.css');
-// module.exports = PickyDateTime;
-
 var PickyDateTime = require('./src/js/PickyDateTime/index.js').default;
 module.exports = PickyDateTime;
