332809260bf663832ac5a636db3a67b7397a4b71
edwardxiao <edwardfhsiao@gmail.com>
2022-02-18 14:10:13 +0800

    no message

--- example/index.js
+++ example/index.js
@@ -3,6 +3,31 @@ import 'unfetch/polyfill';
 import 'abortcontroller-polyfill';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+(function () {
+  if ('performance' in window == false) {
+    window.performance = {};
+  }
+
+  Date.now =
+    Date.now ||
+    function () {
+      // thanks IE8
+      return new Date().getTime();
+    };
+
+  if ('now' in window.performance == false) {
+    var nowOffset = Date.now();
+
+    if (performance.timing && performance.timing.navigationStart) {
+      nowOffset = performance.timing.navigationStart;
+    }
+
+    window.performance.now = function now() {
+      return Date.now() - nowOffset;
+    };
+  }
+})();
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ReactPickyDateTime from '../src/js/component/ReactPickyDateTime';
